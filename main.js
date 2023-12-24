c08e1febccfec89a5fc1c0ba072a8886a412b4cd
Jonathan Moro <jonathan.alejandro.moro@gmail.com>
2023-12-23 23:17:01 -0300

    preEntrega2 array y sus metodos

--- main.js
+++ main.js
@@ -1,30 +1,74 @@
-function venderVideojuego(juego){
-    switch (juego){
-        case "spiderman":
-            return precio = 10;
+
+/* OBJETOS, ARRAYS, METODOS DE ARRAYS, */
+
+
+
+const listaPrendas = [
+    {nombre: "remera", precio: 800, marca: "adidas" },
+    {nombre: "camisa", precio: 300, marca: "pepito" },
+    {nombre: "chaleco", precio: 1600, marca: "moncler" },
+    {nombre: "jean", precio: 900, marca: "gucci" },
+    {nombre: "jogging", precio: 600, marca: "puma" },
+];
+let carrito = []
+
+let eleccion = prompt(`hola bienvenido, desea usted realizar una comprar? si o no?`);
+
+while(eleccion != "si" && eleccion != "no"){
+    alert("por favor ingrese si o no")
+    eleccion = prompt(`hola bienvenido, desea usted realizar una comprar? si o no?`);
+};
+
+if(eleccion == "si"){
+    alert(`Aquí tiene nuestra lista de productos`)
+    let todasLasPrendas = listaPrendas.map(
+        (prenda) => prenda.nombre + " " + prenda.precio + "$"
+    );
+    alert(todasLasPrendas.join(" - "))
+} else if (eleccion == "no" ){
+alert("Gracias por venir, vuelva prontos.")
+};
+
+while(eleccion != "no"){
+    let prenda = prompt("agrega una prenda a tu carrito")
+    let precio = 0
+
+    if(prenda == "remera" || prenda == "camisa" || prenda == "chaleco" || prenda == "jean" || prenda == "jogging"){
+        switch (prenda) {
+            case "remera":
+                precio = 800;
+                break;
+            case "camisa":
+                precio = 300;
+                break;
+            case "chaleco":
+                precio = 1600;
+                break;
+            case "jean":
+                precio = 900;
+                break;
+            case "jogging":
+                precio = 600;
+                break;
+            default:
             break;
-        case "pes":
-            return precio = 20;
-            break;
-        case "fortnite":
-            return precio = 30;
-            break;
-        case "dark souls":
-            return precio = 40;
-            break;
-        default:
-            alert( "juego no disponible, ingrese(spiderman, pes, fortnite, dark souls)")
-            return;
+        }
+    let unidades = parseInt(prompt("Cuantas unidades quiere llevar?"))
+
+    carrito.push({prenda, unidades, precio})
+    console.log(carrito)
+    } else{
+        alert("no tenemos esa prenda")
     }
-}
 
-let precio;
-let saldo = parseInt(prompt("Ingrese su saldo actual"))
-let juego = prompt("ingrese un videojuego")
-let resultado = saldo - venderVideojuego(juego)
+    eleccion = prompt("desea seguir comprando?")
 
-if (precio <= saldo) {alert (`Has comprado ${juego}. Su saldo actual es $${resultado}`);
-}else{
-    alert(`Dinero insuficiente. ${juego} tiene un $${precio}`)
+    while (eleccion === "no"){
+        alert("Gracias por su compra, vuelva prontos.")
+        carrito.forEach((carritoFinal) => {
+            console.log(`prenda: ${carritoFinal.prenda}, unidades: ${carritoFinal.unidades}, precio total a pagar por prenda: $${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+        break;
+    } 
 }
 
